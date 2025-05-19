3a619f6b9f1ec64ad014fa0e85627acc6afbc5ad
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 19:55:21 +0530

    notes

---
+++ shell-practice/06-data-types.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this is comment
+NUMBER1=100
+NUMBER2=200
+
+TIMESTAMP=$(date)
+echo "Script executed at: $TIMESTAMP"
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
---
+++ shell-practice/07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+
+echo "All movies: ${MOVIES[@]}"
